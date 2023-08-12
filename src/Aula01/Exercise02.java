c91df9ddc2f65fd796df35e1dc6bdc331fed2af0
Ângelo Garcia <fhzkngj@gmail.com>
2023-08-12 12:36:37 -0400

    Add Aula01 exercises solved

---
+++ src/Aula01/Exercise02.java
@@ -0,0 +1,17 @@
+package Aula01;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final float PI = 3.14159F;
+
+        System.out.println("Enter the radius:");
+        float radius = input.nextFloat();
+
+        float circleArea = PI * radius * radius;
+
+        System.out.println("The area is " + circleArea);
+    }
+}
