7561f47ed2729c6f4b4e16d546164dd8a3ee9239
Ângelo Garcia <fhzkngj@gmail.com>
2023-08-16 13:06:19 -0400

    Au03Ex01: create Employee class

---
+++ src/Aula03/Exercise01/Employee.java
@@ -0,0 +1,16 @@
+package Aula03.Exercise01;
+
+public class Employee {
+    String name;
+    String department;
+    double salary;
+    String entryDate;
+    String rg;
+
+    void increaseSalary(double increase) {
+        this.salary += increase;
+    }
+    double calcAnnualIncome() {
+        return this.salary * 12;
+    }
+}
