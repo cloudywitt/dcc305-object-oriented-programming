eb731c388d099d918316764edc2b63c9929e3e95
Ângelo Garcia <fhzkngj@gmail.com>
2023-08-17 18:45:15 -0400

    Au03Ex05: add solution

---
+++ src/Aula03/Exercise05/Employee.java
@@ -0,0 +1,23 @@
+package Aula03.Exercise05;
+
+public class Employee {
+    String name;
+    String department;
+    double salary;
+    String entryDate;
+    String rg;
+
+    void increaseSalary(double increase) {
+        this.salary += increase;
+    }
+    double calcAnnualIncome() {
+        return this.salary * 12;
+    }
+    void showAttributes() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Department: " + this.department);
+        System.out.println("Salary: $" + this.salary);
+        System.out.println("Entry Date: " + this.entryDate);
+        System.out.println("RG: " + this.rg);
+    }
+}
