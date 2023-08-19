63b0264d1a7e1823baffd5a6f59d9a721717d862
Ângelo Garcia <fhzkngj@gmail.com>
2023-08-18 20:31:35 -0400

    Au03Ex08: ccpoy ex07 code - the solution is the same

---
+++ src/Aula03/Exercise08/Date.java
@@ -0,0 +1,8 @@
+package Aula03.Exercise08;
+
+public class Date {
+    int day;
+    int month;
+    int year;
+}
+
---
+++ src/Aula03/Exercise08/Employee.java
@@ -0,0 +1,27 @@
+package Aula03.Exercise08;
+
+import java.text.NumberFormat;
+
+public class Employee {
+    String name;
+    String department;
+    double salary;
+    Date entryDate;
+    String rg;
+
+    void increaseSalary(double increase) {
+        this.salary += increase;
+    }
+    double calcAnnualIncome() {
+        return this.salary * 12;
+    }
+    void showAttributes() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+        System.out.println("Name: " + this.name);
+        System.out.println("Department: " + this.department);
+        System.out.println("Salary: " + currency.format(this.salary));
+        System.out.println("Entry Date: " + this.entryDate.day + "/" + this.entryDate.month + "/" + this.entryDate.year);
+        System.out.println("RG: " + this.rg);
+    }
+}
