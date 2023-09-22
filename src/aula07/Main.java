09b80d3833b4222fff937ba832edde050f122534
Ângelo Garcia <fhzkngj@gmail.com>
2023-09-22 08:46:08 -0400

    Aula07: remove main

--- src/aula07/Main.java
+++
@@ -1,9 +0,0 @@
-package aula07;
-
-import java.text.NumberFormat;
-
-public class Main {
-    public static void main(String[] args) {
-        BankAccount checking = new CheckingAccount();
-    }
-}
